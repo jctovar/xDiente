ac03b81bd7f3fe840db7af7e734166c654a6451b
Juan Carlos Tovar Gomez <jctovar@ired.unam.mx>
2015-03-24 16:48:08 -0600

    Directory normalized

---
+++ www/js/app.js
@@ -0,0 +1,84 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+  
+  .state('app.calendar', {
+    url: "/calendar",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/calendar.html",
+          controller: 'CalendarCtrl'
+      }
+    }
+  })
+  
+  .state('app.date', {
+    url: "/date/:dateId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/date.html",
+          controller: 'DateCtrl'
+      }
+    }
+  })
+
+  .state('app.patients', {
+    url: "/patients",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/patients.html",
+          controller: 'PatientsCtrl'
+      }
+    }
+  })
+
+  .state('app.about', {
+    url: "/about",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/about.html",
+          controller: 'AboutCtrl'
+      }
+    }
+  })
+
+  .state('app.patient', {
+    url: "/patient/:patientId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/patient.html",
+        controller: 'PatientCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/calendar');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,131 @@
+angular.module('starter.controllers', ['ngCordova'])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('CalendarCtrl', function($scope, $http) {
+
+	var responsePromise = $http.get("http://xdiente.goritec.com/rest/calendar");
+
+    responsePromise.success(function(data, status, headers, config) {
+        $scope.Dates = data;
+    });
+    
+    responsePromise.error(function(data, status, headers, config) {
+        alert("Error de conexion...!");
+    });
+})
+
+.controller('PatientsCtrl', function($scope, $http) {
+
+	var responsePromise = $http.get("http://xdiente.goritec.com/rest/patients");
+
+    responsePromise.success(function(data, status, headers, config) {
+        $scope.Products = data;
+    });
+    responsePromise.error(function(data, status, headers, config) {
+        alert("Error de conexion...!");
+    });
+})
+
+.controller('DateCtrl', function($scope, $stateParams, $http) {
+
+	console.log("buscando por el id:"+ $stateParams.dateId);
+	
+	// Declara var myData
+    $scope.myData = {};
+	
+	searchPatient($stateParams.dateId);
+	
+	// funcion de buscar producto en el servidor
+	function searchPatient(id){
+		var responsePromise = $http.get("http://xdiente.goritec.com/rest/date/" + id);
+	
+	        responsePromise.success(function(data, status, headers, config) {
+	            $scope.myData.Firstname = data.firstname;
+	            $scope.myData.Lastname = data.lastname;
+	            $scope.myData.Date = data.date;
+	            $scope.myData.Email = data.email;
+	            console.log(id);
+	        });
+	        responsePromise.error(function(data, status, headers, config) {
+	            alert("Error de conexion...!");
+	    });
+	}
+
+})
+
+.controller('PatientCtrl', function($scope, $stateParams, $http) {
+
+	console.log("buscando por el id:"+ $stateParams.patientId);
+	
+	// Declara var myData
+    $scope.myData = {};
+	
+	searchPatient($stateParams.patientId);
+	
+	// funcion de buscar producto en el servidor
+	function searchPatient(id){
+		var responsePromise = $http.get("http://xdiente.goritec.com/rest/patient/" + id);
+	
+	        responsePromise.success(function(data, status, headers, config) {
+	            $scope.myData.Firstname = data.firstname;
+	            $scope.myData.Lastname = data.lastname;
+	            $scope.myData.Address1 = data.address_1;
+	            $scope.myData.Address2 = data.address_2;
+	            $scope.myData.PostalCode = data.postal_code;
+	            $scope.myData.Phone1 = data.phone_1;
+	            $scope.myData.Phone2 = data.phone_2;
+	            $scope.myData.Birthday = data.birthday;
+	            $scope.myData.Email = data.email;
+	        });
+	        responsePromise.error(function(data, status, headers, config) {
+	            alert("Sin conexion...!");
+	    });
+	}
+
+})
+
+.controller('AboutCtrl', function($scope, $cordovaDevice, $cordovaAppVersion) {
+	
+	document.addEventListener("deviceready", function () {
+		
+		$scope.deviceUUID = $cordovaDevice.getUUID();
+	
+		$cordovaAppVersion.getAppVersion().then(function (version) {
+			$scope.appVersion = version;
+		});
+	      
+	}, false);
+
+});
